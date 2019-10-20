ec6b52f674e4116588fc7f8c6f226003b022e3a2
okayu1230z <okayu000z@yahoo.co.jp>
2019-10-20 14:08:09 +0900

    update 20191020

---
+++ test/cread/read/auto_smb_read.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for s in 1 10 20 30 40 50
+do
+	sed -i "415d" /usr/local/bin/fuse_test-master/gdtnfs.cpp &&
+	sed -ie "415i sleep\(${s}\);" /usr/local/bin/fuse_test-master/gdtnfs.cpp &&
+	/usr/local/bin/fuse_test-master/compile.sh /usr/local/bin/fuse_test-master/gdtnfs &&
+	sudo /usr/local/bin/fuse_test-master/mount.sh 
+	for j in 1 10 100 1000 10000 100000 1000000 10000000 100000000
+	do
+		./read_throughput.sh sdfs $j &> smb_sdfs_$s/$s.log
+    done
+	rm /tmp/*
+done
